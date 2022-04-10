ddbb309257fc6648cfedb3e708364da92fae57d0
m.ibrahim <mohammad.si.tt@gmail.com>
2022-04-11 02:08:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const thanksArr = []
+    for (let name of names){
+        thanksArr.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return thanksArr
+}
+function countDown(countNum){
+    while (countNum >= 0){
+        console.log(countNum)
+        countNum--
+    }
+}
